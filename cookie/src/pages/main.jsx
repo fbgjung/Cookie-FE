9a5a6e56a6efe1ad15ce125b996f57a58961021a
euijin <dmlwls7094@naver.com>
2024-11-18 00:07:06 +0900

    Feat :하단네비바 완성 및 헤더,라우터 수정

--- cookie/src/pages/main.jsx
+++ cookie/src/pages/main.jsx
@@ -1,11 +1,30 @@
-import Header from "../components/mypage/Header";
+import Header from "../components/Header";
+import Navbar from "../components/Navvar";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding-bottom: 60px;
+  background-color: #f9f9f9;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  padding: 20px;
+  overflow-y: auto;
+`;
 
 const Main = () => {
   return (
-    <div>
+    <MainContainer>
       <Header />
-      <h1>메인입니다</h1>
-    </div>
+      <Content>
+        <h1>메인입니다</h1>
+      </Content>
+      <Navbar /> {/* Navbar를 항상 하단에 고정 */}
+    </MainContainer>
   );
 };
 
