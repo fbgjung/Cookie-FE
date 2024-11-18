0f3f965043c531dc93ad17c2502ba8da5b736b11
Geonhwi Park <ghpark715@gmail.com>
2024-11-18 17:19:42 +0900

    Feat: Movie Detail 간략한 위치 잡기

--- cookie/src/routes/RouteDef.jsx
+++ cookie/src/routes/RouteDef.jsx
@@ -1,5 +1,10 @@
 import PrivateRoute from "./PrivateRoute";
 import Main from "../pages/main";
+<<<<<<< Updated upstream
+=======
+import Search from "../pages/search";
+import MovieDetail from "../pages/MovieDetail";
+>>>>>>> Stashed changes
 
 export const AppRouteDef = {
   Main: {
@@ -10,4 +15,23 @@ export const AppRouteDef = {
       </PrivateRoute>
     ),
   },
+<<<<<<< Updated upstream
+=======
+  Search: {
+    path: "/search",
+    element: (
+      <PrivateRoute>
+        <Search />
+      </PrivateRoute>
+    ),
+  },
+  MovieDetail: {
+    path: "/movie/:id", // 동적 라우팅
+    element: (
+      <PrivateRoute>
+        <MovieDetail />
+      </PrivateRoute>
+    ),
+  },
+>>>>>>> Stashed changes
 };
