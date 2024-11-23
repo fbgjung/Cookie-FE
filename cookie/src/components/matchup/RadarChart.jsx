fc9460c509781bc82c6d440a05f71cd762d020b7
euijin <dmlwls7094@naver.com>
2024-11-23 16:25:01 +0900

    Feat: 레이더차트 컴포넌트화

---
+++ cookie/src/components/matchup/RadarChart.jsx
@@ -0,0 +1,62 @@
+import { Radar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+} from "chart.js";
+
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip);
+
+const RadarChart = ({ labels, data, backgroundColor, borderColor }) => {
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        label: "포인트1",
+        data,
+        backgroundColor,
+        borderColor,
+        borderWidth: 2,
+        pointBackgroundColor: "#fff",
+        pointBorderColor: borderColor,
+        pointHoverBackgroundColor: borderColor,
+        pointHoverBorderColor: "#fff",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      r: {
+        angleLines: { color: "#ffffff" },
+        grid: { color: "#ffffff" },
+        ticks: {
+          backdropColor: "transparent",
+          color: "#ffffff",
+        },
+        pointLabels: {
+          color: "#ffffff",
+          font: { size: 12 },
+        },
+        suggestedMin: 0,
+        suggestedMax: 10,
+      },
+    },
+    plugins: {
+      tooltip: { enabled: true },
+    },
+    maintainAspectRatio: false,
+  };
+
+  return (
+    <div style={{ width: "250px", height: "250px" }}>
+      {" "}
+      <Radar data={chartData} options={options} />
+    </div>
+  );
+};
+
+export default RadarChart;
