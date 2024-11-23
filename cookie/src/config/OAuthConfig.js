67e0af136f035485adec4c260c9714de4bb50644
ummfieg <ummfieg@naver.com>
2024-11-23 17:43:26 +0900

    feat: config 파일 추가

---
+++ cookie/src/config/OAuthConfig.js
@@ -0,0 +1,13 @@
+// const serverBaseUrl = 'http://cookiekie:8080';
+export const OAuth_LOGIN_PROVIDER = {
+  GOOGLE: {
+    img: googleLogo,
+    url: `${serverBaseUrl}/auth/google`,
+    css: "google",
+  },
+  NAVER: {
+    img: naverLogo,
+    url: `${serverBaseUrl}/auth/naver`,
+    css: "naver",
+  },
+};
