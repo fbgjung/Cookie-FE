270b94a416acf5796096d1295dd88f3bfd2b2c50
Geonhwi Park <ghpark715@gmail.com>
2024-11-25 22:34:10 +0900

    Feat: 경로 수정

--- cookie/src/pages/ReviewFeed.jsx
+++ cookie/src/pages/ReviewFeed.jsx
@@ -10,6 +10,7 @@ const ReviewFeedWrapper = styled.div`
   background-color: #ffffff;
   border-radius: 8px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  height: 100vh;
 `;
 
 const ReviewTitle = styled.h3`
