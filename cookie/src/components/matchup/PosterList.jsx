7a9c1a07c49c131187c5a2a926f5171a823d1c8c
euijin <dmlwls7094@naver.com>
2024-11-25 18:47:01 +0900

    Feat: 투표가 종료되면 투표 못하게 하는 기능 추가

--- cookie/src/components/matchup/PosterList.jsx
+++ cookie/src/components/matchup/PosterList.jsx
@@ -1,11 +1,13 @@
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import Poster from "./Poster";
+import FightIcon from "/src/assets/images/matchup/ic_bigfight.svg";
 
 const PosterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 80px;
+
   margin-top: 30px;
   background-color: #ffffff;
   padding: 30px;
@@ -15,17 +17,49 @@ const PosterContainer = styled.div`
   max-width: 1000px;
 `;
 
-const PosterList = ({ posters }) => (
+const FightImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+`;
+
+const FightImage = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const PosterList = ({ posters, isVoteEnded }) => (
   <PosterContainer>
-    {posters.map((poster) => (
-      <Poster
-        key={poster.movieId}
-        src={poster.src}
-        movieTitle={poster.title}
-        movieId={poster.movieId}
-      />
-    ))}
+    <Poster
+      key={posters[0].movieId}
+      src={posters[0].src}
+      movieTitle={posters[0].title}
+      movieId={posters[0].movieId}
+      isVoteEnded={isVoteEnded}
+    />
+    <FightImageWrapper>
+      <FightImage src={FightIcon} alt="빅매치 아이콘" />
+    </FightImageWrapper>
+    <Poster
+      key={posters[1].movieId}
+      src={posters[1].src}
+      movieTitle={posters[1].title}
+      movieId={posters[1].movieId}
+      isVoteEnded={isVoteEnded}
+    />
   </PosterContainer>
 );
+
+PosterList.propTypes = {
+  posters: PropTypes.arrayOf(
+    PropTypes.shape({
+      movieId: PropTypes.number.isRequired,
+      src: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  isVoteEnded: PropTypes.bool.isRequired,
+};
 
 export default PosterList;
