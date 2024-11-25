1e674382ae3ffeeeb37e7ca6949b46b8621fbcd7
ummfieg <ummfieg@naver.com>
2024-11-25 15:08:56 +0900

    feat: 회원가입페이지 닉네임 중복확인 api연동 완료

--- cookie/src/api/auth/axiosInstance.js
+++ cookie/src/api/auth/axiosInstance.js
@@ -14,21 +14,25 @@ axiosInstance.interceptors.request.use((config) => {
   }
   return config;
 });
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
 
     if (error.response && error.response.status === 401) {
-      if (!originalRequest._retry) {
+      const { message } = error.response.data;
+      if (message === "TOKEN_EXPIRED" && !originalRequest._retry) {
         originalRequest._retry = true;
 
         try {
           const newAccessToken = await refreshAccessToken();
+
           if (newAccessToken) {
             sessionStorage.setItem("accessToken", newAccessToken);
             originalRequest.headers["Authorization"] =
               `Bearer ${newAccessToken}`;
+
             return axiosInstance(originalRequest);
           }
         } catch (refreshError) {
@@ -39,13 +43,34 @@ axiosInstance.interceptors.response.use(
         }
       }
 
-      //  리프레시 토큰도 만료된 경우
       localStorage.clear();
       sessionStorage.clear();
-      navigate("/login");
     }
 
     return Promise.reject(error);
   }
 );
+
+const refreshAccessToken = async () => {
+  try {
+    const refreshToken = sessionStorage.getItem("refreshToken");
+
+    if (!refreshToken) {
+      throw new Error("리프레시 토큰이 존재하지 않습니다.");
+    }
+    const response = await axios.post(
+      `${serverBaseUrl}/api/auth/refresh-token`,
+      {
+        refreshToken,
+      }
+    );
+    return response.data.response.accessToken;
+  } catch (error) {
+    console.error(
+      "리프레시 토큰으로 accessToken을 발급하는데 실패했습니다.",
+      error
+    );
+    throw error;
+  }
+};
 export default axiosInstance;
