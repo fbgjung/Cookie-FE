e1208dd8e68812f0c97c5f5ea8fa1596d9bdb521
euijin <dmlwls7094@naver.com>
2024-11-27 01:40:34 +0900

    Feat:  콘솔 추가:

--- cookie/src/stores/notificationStore.js
+++ cookie/src/stores/notificationStore.js
@@ -1,15 +1,17 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-
 const useNotificationStore = create(
   immer((set) => ({
     notifications: [],
     addNotification: (notification) =>
       set((state) => {
+        console.log("기존 알림 상태:", state.notifications);
         state.notifications.push(notification);
+        console.log("새로운 알림 상태:", state.notifications);
       }),
     clearNotifications: () =>
       set((state) => {
+        console.log("알림 초기화");
         state.notifications = [];
       }),
   }))
