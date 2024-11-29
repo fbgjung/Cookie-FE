2dbde568957dedf6a3c9f795bd0ca5bfacc77a32
euijin <dmlwls7094@naver.com>
2024-11-29 10:03:37 +0900

    Fix: 글로벌 스타일 수정

--- cookie/src/styles/global.js
+++ cookie/src/styles/global.js
@@ -30,15 +30,35 @@ const GlobalStyle = createGlobalStyle`
   }
 
   :root {
-  --main: #04012D;
-  --sub: #1EE5B0;
-  --ticket-bg: #F9F9F9;
-  --sub-btn: rgba(132, 130, 164, 0.12);
-  --match-up: #C4FFEF;
-  --cookie: #CC9D52;
-  --notice: #FF7383;
-  /* color: var(--cookie) */
-}
+    --main: #04012D;
+    --sub: #1EE5B0;
+    --ticket-bg: #F9F9F9;
+    --sub-btn: rgba(132, 130, 164, 0.12);
+    --match-up: #C4FFEF;
+    --cookie: #CC9D52;
+    --notice: #FF7383;
+    /* color: var(--cookie) */
+  }
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden; 
+  }
+
+
+  #root {
+    display: flex;
+    justify-content: center; 
+  }
 `;
 
 export const Content = styled.div`
