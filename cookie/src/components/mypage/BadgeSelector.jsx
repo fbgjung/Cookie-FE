7a7c208534a2863178c2c6c7af67feaf436988fc
euijin <dmlwls7094@naver.com>
2024-11-30 01:03:37 +0900

    Feat: 뱃지 셀렉터트 반응형 추가

--- cookie/src/components/mypage/BadgeSelector.jsx
+++ cookie/src/components/mypage/BadgeSelector.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const BadgeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   margin-top: 20px;
   width: 100%;
 `;
@@ -12,29 +12,49 @@ const BadgeContainer = styled.div`
 const BadgeTitle = styled.h3`
   font-size: 1.2rem;
   font-weight: bold;
-  margin-right: 10px;
-  margin-bottom: 10px;
+  margin-bottom: 10px; /* 아래 간격 추가 */
+  margin-left: 40px;
   text-align: left;
   width: 100%;
   max-width: 450px;
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+    margin-bottom: 8px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 0.9rem;
+    margin-bottom: 5px;
+  }
 `;
 
 const BadgeList = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin-left: 40px;
   align-items: center;
   background-color: #f4f4f4;
   border-radius: 12px;
-  padding: 30px;
+  padding: 80px;
   width: 100%;
-  max-width: 400px;
-  overflow-x: auto;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
+  max-width: 480px;
+  gap: 20px;
 
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-  &::-webkit-scrollbar {
-    display: none;
+  @media (max-width: 768px) {
+    padding: 60px;
+    justify-content: center;
+
+    max-width: 400px;
+    gap: 15px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 50px;
+    justify-content: center;
+    max-width: 300px;
+    gap: 10px;
   }
 `;
 
@@ -49,17 +69,39 @@ const BadgeItem = styled.div`
     width: 60px;
     height: 60px;
     margin-bottom: 8px;
+
+    @media (max-width: 768px) {
+      width: 50px;
+      height: 50px;
+    }
+
+    @media (max-width: 480px) {
+      width: 40px;
+      height: 40px;
+    }
   }
 
   span {
     font-size: 0.9rem;
     color: ${(props) => (props.isSelected ? "#f05454" : "#000")};
     font-weight: ${(props) => (props.isSelected ? "bold" : "normal")};
+
+    @media (max-width: 768px) {
+      font-size: 0.8rem;
+    }
+
+    @media (max-width: 480px) {
+      font-size: 0.7rem;
+    }
   }
 
   input {
     margin-top: 5px;
     accent-color: #f05454;
+
+    @media (max-width: 768px) {
+      margin-top: 3px;
+    }
   }
 
   &:last-child {
@@ -68,10 +110,11 @@ const BadgeItem = styled.div`
 `;
 
 const BadgeSelector = ({ badges, selectedBadge, onBadgeChange }) => {
-  const [currentSelectedBadge, setCurrentSelectedBadge] = useState(selectedBadge);
+  const [currentSelectedBadge, setCurrentSelectedBadge] =
+    useState(selectedBadge);
 
   useEffect(() => {
-    setCurrentSelectedBadge(selectedBadge); // 초기값 설정
+    setCurrentSelectedBadge(selectedBadge);
   }, [selectedBadge]);
 
   const handleBadgeSelect = (badge) => {
