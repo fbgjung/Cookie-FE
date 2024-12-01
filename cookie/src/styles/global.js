9b7354ee1b7becf60a1b3fa1b31cde525d30f0fd
ummfieg <ummfieg@naver.com>
2024-12-02 00:33:37 +0900

    update: 리뷰쓰기 버튼 css 수정

--- cookie/src/styles/global.js
+++ cookie/src/styles/global.js
@@ -46,13 +46,11 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+  
   html, body {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    overflow-x: hidden; 
-  }
+  margin: 0;
+  padding: 0;
+}
 
 
   #root {
