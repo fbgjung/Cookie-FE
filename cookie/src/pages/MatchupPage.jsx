445ca180ec6c3f120eae428184e39ac5389b5f04
euijin <dmlwls7094@naver.com>
2024-12-03 02:15:26 +0900

    Fix: 요청 주소 변경

--- cookie/src/pages/MatchupPage.jsx
+++ cookie/src/pages/MatchupPage.jsx
@@ -113,7 +113,7 @@ const MatchupPage = () => {
       return;
     }
 
-    const socket = new SockJS("http://localhost:8080/ws");
+    const socket = new SockJS("https://cookiekie.com/ws");
     const client = new Client({
       webSocketFactory: () => socket,
       connectHeaders: {
