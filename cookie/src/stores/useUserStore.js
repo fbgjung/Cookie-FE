fd3167fceff89bbb9e35b1f39ab18295c7018bcf
ummfieg <ummfieg@naver.com>
2024-12-04 03:39:42 +0900

    Feat: #104 로그인 시 회원정보 전역관리 스토어 추가

--- cookie/src/stores/useUserStore.js
+++ cookie/src/stores/useUserStore.js
@@ -1,10 +1,41 @@
-// import create from 'zustand';
+import { create } from "zustand";
 
-// const useUserStore = create((set) => ({
-//   userImageUrl: null,
-//   userNickname: '',
-//   setUserImageUrl: (url) => set({ userImageUrl: url }),
-//   setUserNickname: (nickname) => set({ userNickname: nickname }),
-// }));
+const useUserStore = create((set, get) => ({
+  userInfo: {
+    userId: null,
+    nickname: "",
+    profileImage: "",
+    genreId: null,
+  },
 
-// export default useUserStore;
+  // 유저 정보 전체 설정
+  setUserInfo: (info) =>
+    set((state) => ({
+      userInfo: { ...state.userInfo, ...info },
+    })),
+
+  // 닉네임 수정
+  updateNickname: (nickname) =>
+    set((state) => ({
+      userInfo: { ...state.userInfo, nickname },
+    })),
+
+  // 프로필 이미지 수정
+  updateProfileImage: (profileImage) =>
+    set((state) => ({
+      userInfo: { ...state.userInfo, profileImage },
+    })),
+
+  // 장르 ID 수정
+  updateGenreId: (genreId) =>
+    set((state) => ({
+      userInfo: { ...state.userInfo, genreId },
+    })),
+
+  getUserInfo: () => {
+    const state = get();
+    return state.userInfo;
+  },
+}));
+
+export default useUserStore;
