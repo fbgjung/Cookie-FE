405f8d567a57f4cc3f2ff19545b149caaca9f745
euijin <dmlwls7094@naver.com>
2024-12-04 15:27:08 +0900

    Chore:#108 폴더 구조 변경

---
+++ cookie/src/components/common/LoginModal.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import deleteBtn from "../../assets/images/signUp/close_icon.svg";
+import { useNavigate } from "react-router-dom";
+import useAuthStore from "../../stores/useAuthStore";
+
+const BottomModalContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: ${($isOpen) => ($isOpen ? "flex" : "none")};
+  justify-content: center;
+  align-items: flex-end;
+  z-index: 9998;
+`;
+
+const ModalContent = styled.div`
+  border: none;
+  padding: 1rem 2rem 2rem 2rem;
+  margin: calc(100vh - 280px) auto 0 auto;
+  animation: modal-show 0.5s;
+  border-radius: 1rem 1rem 0 0;
+  background-color: var(--ticket-bg);
+  width: 37.5rem;
+  height: 16rem;
+
+  .modal__infoContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.8rem;
+    border-radius: 16px;
+    font-size: 1rem;
+    height: 6.3rem;
+    padding: 0 1.4rem;
+    margin-bottom: 0.5rem;
+    background-color: var(--ticket-bg);
+  }
+
+  .modal__title {
+    color: var(--main);
+  }
+
+  .modal__loginBtn {
+    background-color: var(--main);
+    width: 100%;
+    height: 4rem;
+    font-size: 1rem;
+    border: none;
+    border-radius: 16px;
+    color: white;
+    font-size: 22px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    margin-bottom: 0 0 1rem 0;
+
+    &:hover {
+      background-color: #0b0849;
+    }
+  }
+
+  .modalBtn__container {
+    display: flex;
+    justify-content: end;
+  }
+
+  .modal__closeBtn {
+    background: none;
+    border: none;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  @keyframes modal-show {
+    from {
+      opacity: 0.95;
+      transform: translateY(100%);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;
+
+const LoginModal = () => {
+  const { isLoginModalOpen, closeLoginModal } = useAuthStore();
+  const navigate = useNavigate();
+  if (!isLoginModalOpen) return null;
+
+  const handleModalLogin = () => {
+    closeLoginModal();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <BottomModalContainer $isOpen={isLoginModalOpen}>
+        <ModalContent>
+          <div className="modal">
+            <div className="modalBtn__container">
+              <button className="modal__closeBtn" onClick={closeLoginModal}>
+                <img src={deleteBtn} />
+              </button>
+            </div>
+            <div className="modal__infoContainer">
+              <h2 className="modal__title">로그인이 필요해요 🍪</h2>
+              <p>로그인 후 쿠키를 이용해주세요!</p>
+            </div>
+            <button className="modal__loginBtn bold" onClick={handleModalLogin}>
+              로그인 하기
+            </button>
+          </div>
+        </ModalContent>
+      </BottomModalContainer>
+    </div>
+  );
+};
+
+export default LoginModal;
