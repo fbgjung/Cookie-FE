41134aca1085953c252edbabd0fc386b5b702dbc
euijin <dmlwls7094@naver.com>
2024-12-04 20:31:09 +0900

    Fix:#110 3차테스트

--- cookie/public/firebase-messaging-sw.js
+++ cookie/public/firebase-messaging-sw.js
@@ -1,34 +1,32 @@
-importScripts("https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.21.0/firebase-messaging.js"
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
 
 // Firebase 설정
 const firebaseConfig = {
   apiKey: "AIzaSyB9jvZpOuLYAvGCnSlS2WGpQkXFcK_Ps-g",
-  authDomain: "cookie-6f321.firebaseapp.com",
   projectId: "cookie-6f321",
-  storageBucket: "cookie-6f321.appspot.com",
   messagingSenderId: "840309068007",
   appId: "1:840309068007:web:c41f8e1331c90a23aa1242",
-  measurementId: "G-GGYF1EGC24",
 };
 
 // Firebase 초기화
 firebase.initializeApp(firebaseConfig);
 
-// Messaging 객체 생성
+// Firebase Messaging 객체 초기화
 const messaging = firebase.messaging();
 
-// 백그라운드 메시지 처리
-messaging.onBackgroundMessage((payload) => {
-  console.log("[firebase-messaging-sw.js] 백그라운드 메시지 수신: ", payload);
+// 백그라운드에서 푸시 알림 수신
+messaging.onBackgroundMessage(function (payload) {
+  console.log("백그라운드에서 푸시 알림 받음:", payload);
 
-  const notificationTitle = payload.notification.title || "Default Title";
-  const notificationOptions = {
-    body: payload.notification.body || "Default Body",
-    icon: "/firebase-logo.png", // 알림 아이콘 경로
-  };
+  // 푸시 메시지에서 제목과 본문을 가져옵니다.
+  const { title, body, icon } = payload.notification;
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  // 알림을 화면에 표시
+  self.registration.showNotification(title, {
+    body: body,
+    icon: icon, // 필요에 따라 아이콘 추가
+  });
 });
