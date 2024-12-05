870225425452816bca88d3e17136fef2cd0a8755
euijin <dmlwls7094@naver.com>
2024-12-05 23:09:46 +0900

    Fix: 서비스워커 중복되지 않게로 수정

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -2,15 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
+    .getRegistration("/firebase-messaging-sw.js")
     .then((registration) => {
-      console.log("Service Worker 등록 성공:", registration);
-    })
-    .catch((error) => {
-      console.error("Service Worker 등록 실패:", error);
+      if (registration) {
+        console.log("이미 등록된 Service Worker 있음:", registration);
+      } else {
+        // 새 서비스 워커 등록
+        navigator.serviceWorker
+          .register("/firebase-messaging-sw.js")
+          .then((registration) => {
+            console.log("Service Worker 등록 성공:", registration);
+          })
+          .catch((error) => {
+            console.error("Service Worker 등록 실패:", error);
+          });
+      }
     });
 }
 
