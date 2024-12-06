83155d247f591baff889da6e37e00126272d2793
euijin <dmlwls7094@naver.com>
2024-12-06 11:35:43 +0900

    Fix: 재시도

--- cookie/vite.config.js
+++ cookie/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       manifest: {
         name: "Cookie",
         short_name: "Cookie",
