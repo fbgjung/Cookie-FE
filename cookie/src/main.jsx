d67a7656a25b88a0a6acbf4be68ea785bf95a9a8
euijin <dmlwls7094@naver.com>
2024-12-06 11:00:54 +0900

    Fix: 배포환경단 테스트

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -2,25 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .getRegistration("/firebase-messaging-sw.js")
-    .then((registration) => {
-      if (registration) {
-        console.log("이미 등록된 Service Worker 있음:", registration);
-      } else {
-        // 새 서비스 워커 등록
-        navigator.serviceWorker
-          .register("/firebase-messaging-sw.js")
-          .then((registration) => {
-            console.log("Service Worker 등록 성공:", registration);
-          })
-          .catch((error) => {
-            console.error("Service Worker 등록 실패:", error);
-          });
-      }
-    });
-}
+
+// if ("serviceWorker" in navigator) {
+//   const path = window.location.pathname;
+
+//   // OAuth 관련 경로에서는 서비스 워커 등록 제외
+//   if (!path.includes("/oauth2/") && !path.includes("/login/oauth2/")) {
+//     navigator.serviceWorker
+//       .register("/firebase-messaging-sw.js")
+//       .then((registration) => {
+//         console.log("Service Worker 등록 성공:", registration);
+//       })
+//       .catch((error) => {
+//         console.error("Service Worker 등록 실패:", error);
+//       });
+//   }
+// }
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
