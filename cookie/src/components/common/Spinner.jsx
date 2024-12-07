da27c968d8175b60aee15de8c47fbe15f0dc1279
euijin <dmlwls7094@naver.com>
2024-12-08 00:41:26 +0900

    Feat:#128 네비바 클릭시 호버 상태 추가

---
+++ cookie/src/components/common/Spinner.jsx
@@ -0,0 +1,45 @@
+import styled, { keyframes } from "styled-components";
+import SpinnerImage from "/Users/ejkim/Desktop/Cookie-dev/Cookie-FE/cookie/src/assets/images/cookiespinner.svg";
+
+const spinAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.8);
+  z-index: 9999;
+`;
+
+const SpinnerIcon = styled.img`
+  width: 100px;
+  height: 100px;
+  animation: ${spinAnimation} 1.5s linear infinite;
+
+  @media (max-width: 768px) {
+    width: 70px;
+    height: 70px;
+  }
+`;
+
+const Spinner = () => {
+  return (
+    <SpinnerContainer>
+      <SpinnerIcon src={SpinnerImage} alt="Loading..." />
+    </SpinnerContainer>
+  );
+};
+
+export default Spinner;
