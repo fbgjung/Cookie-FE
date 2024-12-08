506f45df14f767b6e3ea7c30f9bea78cf30de5eb
Geonhwi Park <ghpark715@g.hongik.ac.kr>
2024-12-09 02:31:25 +0900

    feat: Mixpanel SDK 설치

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -1,6 +1,22 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import mixpanel from "mixpanel-browser";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Service Worker 등록 성공:", registration);
+    })
+    .catch((error) => {
+      console.error("Service Worker 등록 실패:", error);
+    });
+}
+
+mixpanel.init("YOUR_PROJECT_TOKEN", {
+  debug: true, // 디버그 활성화
+});
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
