f57138c9e14336d2c7e6c0ef4dae6316440a5d54
fbgjung <fbgjung@gmail.com>
2024-12-10 01:13:54 +0900

    Refactor: #136 메인페이지 AdminRecommend 코드 라인 위치 변경

--- cookie/src/components/main/AdminRecommend.jsx
+++ cookie/src/components/main/AdminRecommend.jsx
@@ -3,6 +3,60 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../api/auth/axiosInstance";
 
+function AdminRecommend() {
+  const navigate = useNavigate();
+  const [recommendMovies, setRecommendMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMainPageMovies = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `/api/movies/mainAdminRecommend`
+        );
+        const recommendMovies = response.data.response;
+        setRecommendMovies(recommendMovies);
+      } catch (error) {
+        console.error("API 호출 오류 발생:", error);
+      }
+    };
+
+    fetchMainPageMovies();
+  }, []);
+
+  const handleMovieClick = (movieId) => {
+    navigate(`/movie/${movieId}`);
+  };
+
+  return (
+    <>
+      <MovieRecommendList>
+        <Title>쿠키 추천 영화</Title>
+        <div className="recommend__movie">
+          {recommendMovies.map((movie, index) => (
+            <div key={index} className="recommend__movie--info">
+              <div onClick={() => handleMovieClick(movie.id)} style={{ cursor: "pointer" }}>
+                <Poster src={movie.poster} alt={movie.title} />
+                <MovieInfo>
+                  <Review>
+                    <ReviewIcon alt="Review Icon"/>
+                    <Count>{movie.reviews}</Count>
+                  </Review>
+                  <Like>
+                    <LikeIcon alt="Review Icon"/>
+                    <Count>{movie.likes}</Count>
+                  </Like>
+                </MovieInfo>
+              </div>
+            </div>
+          ))}
+        </div>
+      </MovieRecommendList>
+    </>
+  );
+}
+
+export default AdminRecommend;
+
 const MovieRecommendList = styled.div`
   .recommend__movie {
     display: flex;
@@ -84,57 +138,3 @@ const Poster = styled.img`
   }
 `;
 
-
-function AdminRecommend() {
-  const navigate = useNavigate();
-  const [recommendMovies, setRecommendMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchMainPageMovies = async () => {
-      try {
-        const response = await axiosInstance.get(
-          `/api/movies/mainAdminRecommend`
-        );
-        const recommendMovies = response.data.response;
-        setRecommendMovies(recommendMovies);
-      } catch (error) {
-        console.error("API 호출 오류 발생:", error);
-      }
-    };
-
-    fetchMainPageMovies();
-  }, []);
-
-  const handleMovieClick = (movieId) => {
-    navigate(`/movie/${movieId}`);
-  };
-
-  return (
-    <>
-      <MovieRecommendList>
-        <Title>쿠키 추천 영화</Title>
-        <div className="recommend__movie">
-          {recommendMovies.map((movie, index) => (
-            <div key={index} className="recommend__movie--info">
-              <div onClick={() => handleMovieClick(movie.id)} style={{ cursor: "pointer" }}>
-                <Poster src={movie.poster} alt={movie.title} />
-                <MovieInfo>
-                  <Review>
-                    <ReviewIcon alt="Review Icon"/>
-                    <Count>{movie.reviews}</Count>
-                  </Review>
-                  <Like>
-                    <LikeIcon alt="Review Icon"/>
-                    <Count>{movie.likes}</Count>
-                  </Like>
-                </MovieInfo>
-              </div>
-            </div>
-          ))}
-        </div>
-      </MovieRecommendList>
-    </>
-  );
-}
-
-export default AdminRecommend;
