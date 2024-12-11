1d60d6834655ad962418ed25b647b00d21b49498
Geonhwi Park <geonhwipark715@gmail.com>
2024-12-11 15:11:49 +0900

    fix: 리뷰에 스포일러 여부 반영 안되는 것 체크용

--- cookie/src/pages/ReviewForm.jsx
+++ cookie/src/pages/ReviewForm.jsx
@@ -181,6 +181,7 @@ const ReviewForm = () => {
       const response = await axiosInstance.post(`/api/reviews`, payload);
       if (response.status === 200) {
         toast.success("리뷰가 성공적으로 등록되었습니다.");
+        console.log("리뷰 등록 성공:", response.data.response);
         navigate("/");
       } else {
         toast.error("리뷰 등록에 실패했습니다. 다시 시도해주세요.");
