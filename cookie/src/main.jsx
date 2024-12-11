eb71c128b5879c0c6ba908e11356d2da8a95ae4a
fbgjung <fbgjung@gmail.com>
2024-12-11 17:31:07 +0900

    Fix: serviceWorker 수정

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -3,14 +3,32 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("/firebase-messaging-sw.js")
+//     .then((registration) => {
+//       console.log("Service Worker 등록 성공:", registration);
+//     })
+//     .catch((error) => {
+//       console.error("Service Worker 등록 실패:", error);
+//     });
+// }
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Service Worker 등록 성공:", registration);
+    .getRegistrations()
+    .then((registrations) => {
+      registrations.forEach((registration) => registration.unregister());
     })
-    .catch((error) => {
-      console.error("Service Worker 등록 실패:", error);
+    .finally(() => {
+      navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then((registration) => {
+          console.log("서비스 워커 등록 성공:", registration);
+        })
+        .catch((error) => {
+          console.error("서비스 워커 등록 실패:", error);
+        });
     });
 }
 
