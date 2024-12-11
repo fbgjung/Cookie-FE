76e1a2f97f07902c671cd9e5ec0b2c27a8147571
euijin <dmlwls7094@naver.com>
2024-12-12 02:04:14 +0900

    HotFix: 오류해결 test 2

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -1,23 +1,16 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .getRegistration("/firebase-messaging-sw.js")
+    .register("/firebase-messaging-sw.js")
     .then((registration) => {
-      if (registration) {
-        console.log("이미 Service Worker가 등록되어 있습니다:", registration);
-      } else {
-        navigator.serviceWorker
-          .register("/firebase-messaging-sw.js")
-          .then((registration) => {
-            console.log("Service Worker 등록 성공:", registration);
-          })
-          .catch((error) => {
-            console.error("Service Worker 등록 실패:", error);
-          });
-      }
+      console.log("Service Worker 등록 성공:", registration);
+    })
+    .catch((error) => {
+      console.error("Service Worker 등록 실패:", error);
     });
 }
 
