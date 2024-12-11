4d2a32bfb6a9f88a127230b1a212d30a99daa9e8
fbgjung <fbgjung@gmail.com>
2024-12-11 17:36:39 +0900

    Hotfix: 서비스 워커 원복

--- cookie/src/main.jsx
+++ cookie/src/main.jsx
@@ -3,32 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("/firebase-messaging-sw.js")
-//     .then((registration) => {
-//       console.log("Service Worker 등록 성공:", registration);
-//     })
-//     .catch((error) => {
-//       console.error("Service Worker 등록 실패:", error);
-//     });
-// }
-
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .getRegistrations()
-    .then((registrations) => {
-      registrations.forEach((registration) => registration.unregister());
+    .register("/firebase-messaging-sw.js")
+    .then((registration) => {
+      console.log("Service Worker 등록 성공:", registration);
     })
-    .finally(() => {
-      navigator.serviceWorker
-        .register("/firebase-messaging-sw.js")
-        .then((registration) => {
-          console.log("서비스 워커 등록 성공:", registration);
-        })
-        .catch((error) => {
-          console.error("서비스 워커 등록 실패:", error);
-        });
+    .catch((error) => {
+      console.error("Service Worker 등록 실패:", error);
     });
 }
 
