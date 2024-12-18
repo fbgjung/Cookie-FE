509f04a294ecaadb95a55ed8d229e5582f5208d5
euijin <dmlwls7094@naver.com>
2024-12-18 17:16:34 +0900

    Hotfix: 뷜드 에러 제거

--- cookie/vite.config.js
+++ cookie/vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import removeConsole from "vite-plugin-remove-console"; // 추가
+// import { VitePWA } from "vite-plugin-pwa"; // PWA 플러그인 주석 처리
 
 export default defineConfig({
-  plugins: [
-    react(),
-    removeConsole({
-      // 옵션 설정: 제거할 로그 타입
-      exclude: ["error", "warn"], // "log"만 제거
-    }),
-  ],
+  plugins: [react()],
   define: {
     global: "window",
   },
