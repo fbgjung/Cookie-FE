149115650a6cc0e36899585c96188770f099a6e6
euijin <dmlwls7094@naver.com>
2024-12-19 17:55:47 +0900

    HotFix: 배포단 콘솔 제거

--- cookie/vite.config.js
+++ cookie/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { VitePWA } from "vite-plugin-pwa"; // PWA 플러그인 주석 처리
 
 export default defineConfig({
   plugins: [react()],
   define: {
     global: "window",
+  },
+  esbuild: {
+    drop: ["console"], // ESBuild 옵션으로 console 제거
   },
 });
